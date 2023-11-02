af5fb6102454713bc92a52c48a159fa696c08419
yeahimjt <80553412+yeahimjt@users.noreply.github.com>
2023-11-01 23:01:59 -0500

    Switching proxy to brightdata

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -16,22 +16,20 @@ export async function scrapeAmazonProduct(url: string) {
 
     // const response = await axios.get(scrapingBeeUrl);
     const username = String(process.env.BRIGHT_DATA_USERNAME);
-  const password = String(process.env.BRIGHT_DATA_PASSWORD);
-  const port = 22225;
-  const session_id = (1000000 * Math.random()) | 0;
+    const password = String(process.env.BRIGHT_DATA_PASSWORD);
+    const port = 22225;
+    const session_id = (1000000 * Math.random()) | 0;
 
-  const options = {
-    auth: {
-      username: `${username}-session-${session_id}`,
-      password,
-    },
-    host: 'brd.superproxy.io',
-    port,
-    rejectUnauthorized: false,
-  }
+    const options = {
+      auth: {
+        username: `${username}-session-${session_id}`,
+        password,
+      },
+      host: 'brd.superproxy.io',
+      port,
+      rejectUnauthorized: false,
+    };
 
-  try {
-    // Fetch the product page
     const response = await axios.get(url, options);
 
     if (response.status !== 200) {
