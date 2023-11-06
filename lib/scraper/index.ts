9787342f46d061acc7c60a85b0f7de605f72bc8a
yeahimjt <80553412+yeahimjt@users.noreply.github.com>
2023-11-06 16:39:57 -0600

    Reverting changes

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -9,23 +9,15 @@ export async function scrapeAmazonProduct(url: string) {
 
   const apiKey = String(process.env.SCRAPERAPI_API_KEY);
   try {
-    const username = String(process.env.BRIGHT_DATA_USERNAME);
-    const password = String(process.env.BRIGHT_DATA_PASSWORD);
-    const port = 22225;
-    const session_id = (1000000 * Math.random()) | 0;
+    // Set up the ScrapingBee API request
+    const scrapingBeeUrl = `http://api.scraperapi.com?api_key=${apiKey}&url=${encodeURIComponent(
+      url
+    )}`;
 
-    const options = {
-      auth: {
-        username: `${username}-session-${session_id}`,
-        password,
-      },
-      host: 'brd.superproxy.io',
-      port,
-      rejectUnauthorized: false,
-    };
-    const response = await axios.get(url, options);
+    const response = await axios.get(scrapingBeeUrl);
+
     if (response.status !== 200) {
-      console.log(`Failed to scrape product - HTTP Status: ${response}`);
+      console.log(`Failed to scrape product - HTTP Status: ${response.status}`);
       return null;
     }
 
